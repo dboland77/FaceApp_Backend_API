c21f7782c78d83c4bc5439d501eb9bbb9d9aa587
David Boland <dboland77@gmail.com>
2020-09-15 14:34:25 +0100

    changed config

--- config.js
+++ config.js
@@ -1,12 +1,32 @@
 const Pool = require("pg").Pool;
-const dotenv = require('dotenv');
+const dotenv = require("dotenv").config();
 
-const pool = new Pool({
-    user: "postgres",
-    password:"",
-    database:process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT
-});
+const env = process.env.NODE_ENV;
+
+let connectionString = {
+  user: "postgres",
+  password: "",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+};
+
+console.log(connectionString, env);
+
+// Check which environment (dev or production) and decide
+// on the connection string to use
+
+console.log(env, env === "development");
+
+if (env === "development") {
+  connectionString.database = process.env.DB_DATABASE;
+} else {
+  connectionString = {
+    connectionString: process.env.DATABASE_URL,
+  };
+}
+
+console.log(connectionString);
+
+const pool = new Pool(connectionString);
 
 module.exports = pool;
